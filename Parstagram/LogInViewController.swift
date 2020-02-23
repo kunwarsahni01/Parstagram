acffe748263b4b6316fc1b6c0376151a28639673
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-02-23 16:37:36 -0500

    Added Login and Feed Controllers

---
+++ Parstagram/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  Parstagram
+//
+//  Created by Kunwar Sahni on 2/23/20.
+//  Copyright © 2020 purdue. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
